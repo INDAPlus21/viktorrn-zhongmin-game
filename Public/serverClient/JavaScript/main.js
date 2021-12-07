2b7c6da10c17c80eb8cd29b0426804623d5fe68e
viktorrn <90602664+viktorrn@users.noreply.github.com>
2021-12-07 21:08:03 +0100

    done work on client page

---
+++ Public/serverClient/JavaScript/main.js
@@ -0,0 +1,31 @@
+
+window.onload = function(){
+  
+}
+
+//neccessary Util functions
+export function $(el) { return document.getElementById(el) };
+export function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+export function clearElement(el){
+    while(el.firstChild){
+        el.removeChild(el.firstChild);
+    }
+}
+export function getUIHandler(){
+    return UI_Handle;
+}
+export function getCardLib(){
+    return CardLibrary;
+}
+export function cloneObject(obj){
+    return JSON.parse(JSON.stringify(obj));
+}
