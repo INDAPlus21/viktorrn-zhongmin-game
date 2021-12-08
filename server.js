f58403f0053baae96e6686e89923bae9019ab1b7
viktorrn <90602664+viktorrn@users.noreply.github.com>
2021-12-08 22:26:16 +0100

    added JSON file and folder for card Data

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // the file to run from node
-//const express = require('express') // create module
-//let app = express(); // create an express object
+const express = require('express') // create module
+let app = express(); // create an express object
 const http = require('http').Server(app);
 
-//app.use(express.static(__dirname + '/Public')); 
+app.use(express.static(__dirname + '/Public')); 
 
 http.listen(5000, function(){ // set port
     console.log('listening on *:5000');
