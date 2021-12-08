3c4c1533ec5a238d9e9d13275e521af5acebcab8
viktorrn <90602664+viktorrn@users.noreply.github.com>
2021-12-08 21:43:57 +0100

    did some more work on serverClient Page and started prepping for handling players

---
+++ Public/baseClasses/cardLib.js
@@ -0,0 +1,25 @@
+import * as Main from '../playerClient/JavaScript/main.js';
+export class CardLib{
+    
+    constructor(filePath){
+        this.library = new Array();
+        this.readCardsFromJSONFile(filePath);
+    }
+
+    readCardsFromJSONFile(filePath){
+
+    }
+
+    extractCardData(){
+
+    }
+
+    getGameTable(){
+
+    }
+
+    getSpecificCard(name){
+        
+    }
+
+}
