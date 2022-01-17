84036a24b9798b42881cbd8b167d1a1a29bf6808
viktorrn <90602664+viktorrn@users.noreply.github.com>
2022-01-17 13:12:05 +0100

    added basis for AI implementation

---
+++ Public/hostClient/JavaScript/vixiAI.js
@@ -0,0 +1,9 @@
+export function takeTurn(playerInfo,boardInfo,turn){
+    let actionList = [];
+    let hand = playerInfo.player1.hand;
+    let blood = playerInfo.player1.blood;
+    let yourBoard = boardInfo.player1;
+    let enemyBoard = boardInfo.player2;
+
+
+}
